6b636e4ece76bbca2f733d64d5be2250c8d61cfc
Endour <contact@endour.tech>
2024-01-03 18:29:59 +0100

    REPO: init repo

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1>FR</h1>
+
+# Clonez l'application pour proposez votre code.
+
+### Vous avez besoin d'installer NodeJs
+## Windows : https://nodejs.org/dist/v19.1.0/node-v19.1.0-x64.msi
+
+### Dépendance nécessaire :
+ElectronJs, Electron Builder, DiscordRPC, Electron Updates, Electron Log
+
+Installez les en faisant
+```
+npm install
+```
+
+### Vous devez faire quelque chagement :
+```
+Dans src/config/config.demo.json d'abord le renommer config.json puis entrez les bonnes valeurs
+```
+
+### Démarrez l'application en faisant
+````
+npm run start
+````
---
+++ index.js
@@ -0,0 +1,65 @@
+// We import all modules here.
+const { app, BrowserWindow } = require("electron");
+const RPC = require("discord-rpc");
+const config = require("./src/config/config.json");
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+// Main function. He define title and others stuff.
+function createWindow () {
+  const win = new BrowserWindow({
+    title: "YouTube Browser",
+    icon: __dirname + './src/icons/logotYT.ico',
+    autoHideMenuBar: true,
+    width: 1280,
+    height: 720,
+  });
+
+  win.loadURL("https://youtube.com/");
+
+};
+
+// We create main window here.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    };
+  });
+});
+
+app.on("ready", () => {
+
+  rpc.on("ready", () => {
+
+    // We set button.
+    rpc.setActivity({
+      buttons: [
+        { label : `Rejoignez le support`, url: `${config.discord}`}
+    ],
+
+      // We set all details for your rpc.
+      details: `Regarde des vidéos`,
+      startTimestamp: new Date(),
+      largeImageKey: "youtube",
+      largeImageText: "Le divertissement est un bien essentiel."
+        
+        
+    });
+  });
+
+    // Connect to Discord with discord-rpc module
+    rpc.login({ 
+      clientId: config.clientId,
+    });
+
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  };
+});
