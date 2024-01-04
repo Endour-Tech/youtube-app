8705bb38972689fd22a7cd4e0c10f972a5323561
Endour <contact@endour.tech>
2024-01-04 23:02:20 +0100

    APP: update rpc for add the title when you hover the youtube logo

--- index.js
+++ index.js
@@ -13,16 +13,40 @@ updateElectronApp();
 // Main function. He define title and others stuff.
 function createWindow () {
   const win = new BrowserWindow({
-    title: "YouTube",
-    icon: path.join(__dirname, 'src/icons/logo.ico'), // use absolute path and include file extension
-    autoHideMenuBar: true,
     width: 1280,
     height: 720,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+    }
   });
 
   win.loadURL("https://www.youtube.com/");
 
-};
+  let i = 0;
+
+  setInterval(() => {
+    win.webContents.on('page-title-updated', () => {
+      win.webContents.executeJavaScript('document.title')
+        .then(title => {
+          global.title = title; // set the title as a global variable
+        });
+    });
+  }, 30000); // 60000 millisecondes = 1 minute
+
+  // while (i < 1) {
+  //   win.webContents.on('did-finish-load', () => {
+  //     win.webContents.executeJavaScript('document.title')
+  //       .then(title => {
+  //         global.title = title; // set the title as a global variable
+  //         console.log(title * 1); // log the title
+  //       });
+  //   });
+  // }
+
+}
 
 // We create main window here.
 app.whenReady().then(() => {
@@ -39,26 +63,30 @@ app.on("ready", () => {
 
   rpc.on("ready", () => {
 
-    // We set button.
-    rpc.setActivity({
-      buttons: [
-        { label : `Rejoignez le support`, url: `${config.discord}`}
-    ],
 
-      // We set all details for your rpc.
-      details: `Regarde des vidéos`,
-      startTimestamp: new Date(),
-      largeImageKey: "youtube",
-      largeImageText: "Le divertissement est un bien essentiel."
-        
-        
-    });
+    setInterval(() => {
+      rpc.setActivity({
+        buttons: [
+          { label : `Rejoignez le support`, url: `${config.discord}`}
+      ],
+  
+        // We set all details for your rpc.
+        details: `Regarde des vidéos`,
+        startTimestamp: new Date(),
+        largeImageKey: "youtube",
+        largeImageText: `${global.title}`
+          
+          
+      });
+    }, 32000);
+    // We set button.
   });
 
-    // Connect to Discord with discord-rpc module
+  setInterval(() => {
     rpc.login({ 
       clientId: config.clientId,
     });
+  }, 31000);
 
 });
 
